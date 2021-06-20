8bd7c376cd3fa155b3688d677669c0a3f776b5c2
jvalero <jorgevm13@gmail.com>
2021-06-20 22:47:48 +0200

    WIP angular forms

--- src/app/digimon/main-page/main-page.component.ts
+++ src/app/digimon/main-page/main-page.component.ts
@@ -1,4 +1,7 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+
+import { Digimon } from '../../models/Digimon';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
+  digimon: Digimon = {
+    name: '',
+    type: ''
+  };
+
+  digimons: Digimon[] = [
+    {
+      name: 'Agumon',
+      type: 'fire'
+    },
+    {
+      name: 'Gabumon',
+      type: 'fire'
+    },
+    {
+      name: 'Patamon',
+      type: 'flying'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  add(){
+    if(this.digimon.name.trim().length === 0 && this.digimon.type.trim().length === 0){
+      return;
+    }
+    console.log(this.digimon);
+    this.digimons.push(this.digimon);
+    this.digimon = {
+      name: '',
+      type: ''
+    }
+  }
 }
---
+++ src/app/models/Digimon.ts
@@ -0,0 +1,5 @@
+export interface Digimon {
+    name: string;
+    type: string;
+  }
+  
